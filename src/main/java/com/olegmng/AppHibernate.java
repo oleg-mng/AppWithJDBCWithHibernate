0f107131c7a4d3ac282c9c0e4b742b1a822dd07f
oleg <legik7127@gmail.com>
2023-12-26 15:30:16 +0300

    Added changes

--- src/main/java/com/olegmng/AppHibernate.java
+++ src/main/java/com/olegmng/AppHibernate.java
@@ -4,34 +4,56 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.LongStream;
+
 public class AppHibernate {
     public static void main(String[] args) {
+
         final SessionFactory sessionFactory = new Configuration().
                 configure("hibernate.cfg.xml").buildSessionFactory();
-        try (Session session = sessionFactory.openSession()){
+        try (Session session = sessionFactory.openSession()) {
             session.beginTransaction();
 
-            User user = new User();
-            user.setName("Oleg");
-            System.out.println("User before persist: " + user.getId());
+            List<User> users = LongStream.rangeClosed(1, 10)
+                    .mapToObj(it -> new User(it))
+                    .peek(it -> it.setName("User #" + it.getId()))
+                    .peek(it -> session.persist(it))
+                    .collect(Collectors.toList());
 
-            session.persist(user);
-            System.out.println("User after persist: " + user.getId());
+
+//            User user = new User();
+//            user.setName("Oleg");
+//            System.out.println("User before persist: " + user.getId());
+//
+//            session.persist(user);
+//            System.out.println("User after persist: " + user.getId());
             session.getTransaction().commit();
 
         }
 
-        try (Session session = sessionFactory.openSession()){
+        //операция update
+        try (Session session = sessionFactory.openSession()) {
             session.beginTransaction();
 
-            User loadedUser = (User) session.get(User.class, 1);
-            System.out.println("User = " + loadedUser);
+            User loadedUser = session.get(User.class, 1);
+            loadedUser.setName("Updated");
+
+            session.merge(loadedUser);
+            session.getTransaction().commit();
+
         }
 
-        try (Session session = sessionFactory.openSession()){
-            session.beginTransaction();
+        try (Session session = sessionFactory.openSession()) {
 
-            User loadedUser = (User) session.get(User.class, 1);
+            //JQL - java query language
+            List<User> users = session.createQuery("select u from User u where id >= 1", User.class)
+                    .getResultList();
+
+            System.out.println(users);
+
+            User loadedUser = session.get(User.class, 1);
             System.out.println("User = " + loadedUser);
         }
 
--- src/main/java/com/olegmng/User.java
+++ src/main/java/com/olegmng/User.java
@@ -5,13 +5,18 @@ import jakarta.persistence.*;
 @Entity
 @Table(name = "users")
 public class User {
+    public User() {
+    }
+
+    public User(Long id) {
+        this.id = id;
+    }
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+//    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     @Column(name = "name")
     private String name;
-
 
     public void setId(Long id) {
         this.id = id;
